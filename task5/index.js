225137d1fb8f9307305edf113d7955f7f938deb4
Tadashi Konno <jensonnumber1@gmail.com>
2020-07-03 16:18:30 -0700

    task5 pc version done

--- task5/index.js
+++ task5/index.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-    $('.nav_li').hover(
+    /*サブメニューがないときは黒のオーバーレイをかけない*/
+    var nav_li = $('.nav_li').has('.sub_menu_wrapper');
+    $(nav_li).hover(
         function() {
-            $('.overlay').css('display', 'block');
+                $('.overlay').css('display', 'block');
         },
         function() {
-            $('.overlay').css('display', 'none');
+                $('.overlay').css('display', 'none');
         }
     )
 })
