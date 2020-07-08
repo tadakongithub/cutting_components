a4ff8ab40379df9218885da4c35f7cfe4441dd1e
Tadashi Konno <jensonnumber1@gmail.com>
2020-07-07 23:27:05 -0700

    task5 mobile done

--- task5/index.js
+++ task5/index.js
@@ -8,5 +8,33 @@ $(document).ready(function() {
         function() {
                 $('.overlay').css('display', 'none');
         }
-    )
+    );
+
+    //バーガーメニューがクリックされたらモバイル用ヘッダー表示
+    $('.hamburger').on('click', function() {
+        $('.mobile_header').css('display', 'block');
+    });
+
+    //モバイルの各セクションがクリックされたらサブメニューを表示
+    $('.mobile_header_section').on('click', function() {
+        if($(this).find('.accordion_body').css('display') === 'block'){
+            $('.accordion_body').css('display', 'none');
+            $('.mobile_arrow').css('transform', 'rotate(-45deg)');
+        } else {
+            $('.accordion_body').css('display', 'none');
+            $('.mobile_arrow').css('transform', 'rotate(-45deg)');
+            $(this).find('.accordion_body').css('display', 'block');
+            $(this).find('.mobile_arrow').css('transform', 'rotate(-225deg)');
+        }
+    });
+
+    //ハンバーガーがクリックされたらメニューが出てくる
+    $('.hamburger').on('click', function() {
+        $('.mobile_header').css('transform', 'translateY(0)');
+    });
+
+    //バッテンクリックでメニュー閉じる
+    $('.close_btn').on('click', function() {
+        $('.mobile_header').css('transform', 'translateY(-100%)');
+    });
 })
