d8d5996f0f2e238d1b56293b7515f05def8b685d
Tadashi Konno <jensonnumber1@gmail.com>
2020-07-27 12:30:59 -0700

    task13 done

--- task13/index.js
+++ task13/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('.to_top').on('click', function(e){
-        $('body, html').animate({'scrollTop': 0}, 1000);
+        $('body, html').animate({'scrollTop': 0}, 500);
     });
 });
